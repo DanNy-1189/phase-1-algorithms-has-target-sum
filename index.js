7d6fb109a2de8f33aa8d120aea2552118b29a881
Nancy D. Arena <ndarena11@gmail.com>
2023-07-10 16:12:34 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {  
+    let currentNum = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      let compareNum = array[j];
+      if (currentNum === compareNum) {
+        return true;
+      }
+    }
+  }
+  return false
 }
 
 /* 
